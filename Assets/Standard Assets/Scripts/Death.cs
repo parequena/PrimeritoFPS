71fcc578a9328e97bd73a4f841cca3e36f5f8407
Pablo <parequena8@gmail.com>
2019-10-09 23:05:28 +0200

    FPS 09/10/2019. Hecho en clase

---
+++ Assets/Standard Assets/Scripts/Death.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Death : MonoBehaviour {
+
+    public AudioClip m_deathSound;
+
+    private AudioSource m_audio;
+    private GameObject m_GameManager;
+
+    void Start()
+    {
+        m_audio = GetComponent<AudioSource>();
+        // TO-DO 1 Buscar al GameManager y cachearlo
+    }
+
+    public virtual void OnDeath()
+    {
+
+        m_audio.clip = m_deathSound;
+        m_audio.Play();
+        // TO-DO 2 Respaunear usando el GameManager con el mensaje RespawnPlayer.
+    }
+}
---
+++ Assets/Standard Assets/Scripts/HealthSlider.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthSlider : MonoBehaviour {
+
+
+    public Health m_health;
+
+    private UnityEngine.UI.Slider m_slider;
+
+	// Use this for initialization
+	void Start () {
+
+        //TO-DO 1 Cargar HealthSlider y configurarlo con valores por defecto
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //TO-DO 2 leer el valor del health actual
+    }
+}
